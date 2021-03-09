5dbc65b897d3344d8194d55c4214a0608ad69793
farhanshahriyarabir62 <71967231+farhanshahriyarabir62@users.noreply.github.com>
2021-03-09 15:25:26 +0600

    first commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const ytdl = require('ytdl-core');
 const { YTSearcher } = require('ytsearcher');
 
 const searcher = new YTSearcher({
-    key : process.env,youtube_api,
+    key : process.env.youtube_api,
     revealed: true
 });
 
