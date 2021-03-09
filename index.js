e347eef78024d91bc65e10ca981823247485d491
farhanshahriyarabir62 <71967231+farhanshahriyarabir62@users.noreply.github.com>
2021-03-09 15:16:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,113 @@
+const { executionAsyncResource } = require('async_hooks');
+const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
+
+const { YTSearcher } = require('ytsearcher');
+
+const searcher = new YTSearcher({
+    key : "AIzaSyDDfhhfsPCYzTnnI7uhzDj0tsh9NJLJKjE",
+    revealed: true
+});
+
+
+const client = new Discord.Client();
+
+const queue = new Map();
+
+client.on("ready", () => {
+    console.log("I am online!")
+})
+
+client.on("message", async(message) => {
+    const prefix = '+';
+
+    const serverQueue = queue.get(message.guild.id);
+ 
+    const args = message.content.slice(prefix.length).trim().split(/ +/g)
+    const command = args.shift().toLowerCase();
+ 
+    switch(command){
+        case 'play':
+            execute(message, serverQueue);
+            break;
+        case 'stop':
+            stop(message, serverQueue);
+            break;
+        case 'skip':
+            skip(message, serverQueue);
+            break;
+    }
+ 
+    async function execute(message, serverQueue){
+        let vc = message.member.voice.channel;
+        if(!vc){
+            return message.channel.send("Aww honey please join voice first! Thank You. ");
+        }else{
+            let result = await searcher.search(args.join(" "), { type: "video" })
+            const songInfo = await ytdl.getInfo(result.first.url)
+ 
+            let song = {
+                title: songInfo.videoDetails.title,
+                url: songInfo.videoDetails.video_url
+            };
+ 
+            if(!serverQueue){
+                const queueConstructor = {
+                    txtChannel: message.channel,
+                    vChannel: vc,
+                    connection: null,
+                    songs: [],
+                    volume: 10,
+                    playing: true
+                };
+                queue.set(message.guild.id, queueConstructor);
+ 
+                queueConstructor.songs.push(song);
+ 
+                try{
+                    let connection = await vc.join();
+                    queueConstructor.connection = connection;
+                    play(message.guild, queueConstructor.songs[0]);
+                }catch (err){
+                    console.error(err);
+                    queue.delete(message.guild.id);
+                    return message.channel.send(`**I cant able to join the voice chat. Can you check what is the problem dear? if you are not Abir thn please tell him to fix me asap!** ${err}`)
+                }
+            }else{
+                serverQueue.songs.push(song);
+                return message.channel.send(`Your song has been added dear ${song.url}`);
+            }
+        }
+    }
+    function play(guild, song){
+        const serverQueue = queue.get(guild.id);
+        if(!song){
+            serverQueue.vChannel.leave();
+            queue.delete(guild.id);
+            return;
+        }
+        const dispatcher = serverQueue.connection
+            .play(ytdl(song.url))
+            .on('finish', () =>{
+                serverQueue.songs.shift();
+                play(guild, serverQueue.songs[0]);
+            })
+            serverQueue.txtChannel.send(`**Now playing** - ${serverQueue.songs[0].url}`)
+           
+    }
+    function stop (message, serverQueue){
+        if(!message.member.voice.channel)
+            return message.channel.send("Aww honey please join voice first! Thank You.")
+        serverQueue.songs = [];
+        serverQueue.connection.dispatcher.end();
+    }
+    function skip (message, serverQueue){
+        if(!message.member.voice.channel)
+            return message.channel.send("Aww honey please join voice first! Thank You.");
+        if(!serverQueue)
+            return message.channel.send("There is nothing to skip honey!");
+        serverQueue.connection.dispatcher.end();
+    }
+})
+
+client.login("ODE4MTczNDk1MzA0MjU3NjA2.YEUNcg.KQaa-EMHp0nmhxbPidcrnMxoEhQ")
